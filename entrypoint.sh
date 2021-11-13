10116fbd402e0423a77471817baaa43227c48fa1
remotephone <remotephone@gmail.com>
2021-11-12 22:58:37 -0600

    fixing port

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
 #!/usr/bin/env bash
 # start-server.sh
 python manage.py migrate --noinput
-(cd /usr/src/app; gunicorn content_aggregator.wsgi --user www-data --bind 0.0.0.0:8000 --workers 3) &
+(cd /usr/src/app; gunicorn content_aggregator.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
 
